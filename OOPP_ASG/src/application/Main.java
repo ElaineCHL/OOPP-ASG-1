f53d7db6faf2df84007b76f17afc47c3ef23fe18
ElaineCHL <140783221+ElaineCHL@users.noreply.github.com>
2024-04-19 07:01:48 +0800

    Update Main.java

--- OOPP_ASG/src/application/Main.java
+++ OOPP_ASG/src/application/Main.java
@@ -12,5 +12,7 @@ public class Main  {
 	public static void main(String[] args) {
 		System.out.println("Hello world adsfFX");
 		System.out.println("Testingg");
+		System.out.println("Testingg");
+
 	}
 }
