8a828c0f03dce0b5a16c2114cc29134191720f6d
ElaineCHL <elainechunghuilin@gmail.com>
2024-04-19 16:55:44 +0800

    asdf

--- OOPP_ASG/src/application/Main.java
+++ OOPP_ASG/src/application/Main.java
@@ -1,10 +1,4 @@
 package application;
-	
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-
 
 public class Main  {
 
@@ -12,6 +6,5 @@ public class Main  {
 	public static void main(String[] args) {
 		System.out.println("Hello world adsfFX");
 		System.out.println("Testingg");
-
 	}
 }
