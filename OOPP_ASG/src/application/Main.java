2f7a3ed596e41e1601956b266f12d90da7cb1112
Mijiiii MC <Mijiiii MC@LAPTOP-N49097UD>
2024-04-19 16:35:40 +0800

    asdf

--- OOPP_ASG/src/application/Main.java
+++ OOPP_ASG/src/application/Main.java
@@ -12,6 +12,8 @@ public class Main  {
 	public static void main(String[] args) {
 		System.out.println("Hello world adsfFX");
 		System.out.println("Testingg");
-
+		System.out.println("helo");
+		System.out.println("jel");
+		System.out.println("  ");
 	}
 }
