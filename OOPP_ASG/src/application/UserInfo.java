454bc5a48101d5c76767ed73973f7d9b74bbbdb4
Mijiiii MC <Mijiiii MC@LAPTOP-N49097UD>
2024-04-20 11:57:48 +0800

    Updates: 1)Product class 2)Microwave class 3)UserInfo class

---
+++ OOPP_ASG/src/application/UserInfo.java
@@ -0,0 +1,77 @@
+package application;
+
+
+public class UserInfo {
+
+	private String Name;
+	private int IC;
+	private int HpNo;
+	private String Email;
+	private String UserID;
+	
+	//Default Constructor
+	public UserInfo() {
+		
+	}
+	
+	public UserInfo(String nm, int ic, int hpno, String em, String uid) {
+		Name=nm;
+		IC=ic;
+		HpNo=hpno;
+		Email=em;
+		UserID=uid;
+	}
+
+	//Set & Get Name
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+
+	//Set & Get IC
+	public int getIC() {
+		return IC;
+	}
+
+	public void setIC(int iC) {
+		IC = iC;
+	}
+	
+	//Set & Get HpNo
+	public int getHpNo() {
+		return HpNo;
+	}
+
+	public void setHpNo(int hpNo) {
+		HpNo = hpNo;
+	}
+
+	//Set & Get Email
+	public String getEmail() {
+		return Email;
+	}
+
+	public void setEmail(String email) {
+		Email = email;
+	}
+
+	//Set & Get UserID
+	public String getUserID() {
+		return UserID;
+	}
+
+	public void setUserID(String userID) {
+		UserID = userID;
+	}
+	
+	//ToString
+	public String toString() {
+		return ("Name: "+Name+"IC: "+IC+"PhoneNo: "+HpNo+"Email: "+Email+"UserID: "+UserID);
+	}
+	
+	
+}
+
---
+++ OOPP_ASG/src/application/Warranty.java
@@ -0,0 +1,22 @@
+package application;
+
+
+public class Warranty {
+
+    private static int duration;//in Years
+    
+    public Warranty() {
+    	Warranty.duration=0;
+    }
+	
+    public Warranty(int duration) {
+
+    	Warranty.duration=duration;
+    }
+    
+    public static int getDuration() {
+    	return duration;
+    }
+	
+}
+
