9df4b11e08afc1d42cb5bcc505636091d5820dc8
ElaineCHL <elainechunghuilin@gmail.com>
2024-04-22 11:17:01 +0800

    little changes

--- OOPP_ASG/src/application/UserInfo2.java
+++ OOPP_ASG/src/application/UserInfo2.java
@@ -8,27 +8,13 @@ public class UserInfo2 {
 	private String userID;
 
 	public void promptName() {
-//		boolean contains_space = false;
 		System.out.println("Please enter your full name: ");
 		name = sc.nextLine();
 		int index = name.lastIndexOf(' ');
 
-//		for (int i = 0; i < name.length(); i++) {
-//			if ((name.charAt(i)) == ' ') {
-//				contains_space = true;
-//				break;
-//			}
-//		}
-//		if (contains_space) {
-//			userID = name.charAt(1) + name.substring(name.lastIndexOf(" ") + 1);
-//		} else {
-//			userID = "guest";
-//		}
-		
-		if (index == -1) { // 
+		if (index == -1) {
 			userID = "guest";
-		}
-		else {
+		} else {
 			userID = name.charAt(0) + name.substring(index + 1);
 		}
 	}
